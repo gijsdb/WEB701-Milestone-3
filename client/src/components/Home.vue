33d97ede5c2ba8ef409b67986f80b9f981b06235
gijsdb <gijsdeblauw@gmail.com>
2020-05-19 15:33:48 +1200

    set up new vue client application

---
+++ client/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+          <h1>Hello</h1>
+      </div>
+      <div class="col-4">
+         <h1>Hi</h1>
+      </div>
+      <div class="col-4">
+        <h1>Bye</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
